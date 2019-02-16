24c859ad31f2f480ba40c8259697fedafb82b404
이재준 <asapz@naver.com>
2019-02-16 11:32:26 +0900

    13 finish

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,41 +1,41 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Counter.css';
 
-const Counter = ({number,color,onIncrement,onDecrement,onSetColor}) =>{
-    return (
-       <div 
-        className="Counter"
-        onClick={onIncrement}
-        //onContextMenu란 마우스 오른쪽 버튼을 눌렀을 때 메뉴가 열리는 이벤트를 의미한다.
-        onContextMenu={(e)=>{
-            e.preventDefault(); //메뉴가 열리는 것을 방지한다.
-            onDecrement();        
-        }}
-        onDoubleClick={onSetColor}
-        style={{
-            backgroundColor:color
-        }}>
-        {number}
-        </div>
-    );
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
+  return (
+    <div
+      className="Counter"
+      onClick={() => onIncrement(index)}
+      onContextMenu={(e) => {
+      e.preventDefault();
+      onDecrement(index);
+    }}
+      onDoubleClick={() => onSetColor(index)}
+      style={{
+      backgroundColor: color
+    }}>
+      {number}
+    </div>
+  );
 };
 
-//propTypes를 지정해준다.
 Counter.propTypes = {
-    number:PropTypes.number,
-    color:PropTypes.string,
-    onIncrement:PropTypes.func,
-    onDecrement:PropTypes.func,
-    onSetColor:PropTypes.func
+  index: PropTypes.number,
+  number: PropTypes.number,
+  color: PropTypes.string,
+  onIncrement: PropTypes.func,
+  onDecrement: PropTypes.func,
+  onSetColor: PropTypes.func
 };
 
 Counter.defaultProps = {
-    number:0,
-    color:'black',
-    onIncrement: () => console.warn('onIncrement not defined'),
-    onDecrement: () => console.warn('onDecrement not defined'),
-    onSetColor: () => console.warn('onSetColor not defined')
+  index: 0,
+  number: 0,
+  color: 'black',
+  onIncrement: () => console.warn('onIncrement not defined'),
+  onDecrement: () => console.warn('onDecrement not defined'),
+  onSetColor: () => console.warn('onSetColor not defined')
 };
 
 export default Counter;
